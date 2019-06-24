50e4f0e2f9406bfdc823c7bce689d43bff30e527
Chen Wei <chenwei@byzantine-lab.io>
2019-06-24 13:41:15 +0800

    init commit

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const Test = styled.div`
+  border: 1px solid red;
+  height: 20px;
+`;
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Test />
+    <h1>Hi people testest 2222</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
