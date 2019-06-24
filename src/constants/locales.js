72f5ca416429d2a53f9e3e029936094c2bf55ea8
Chen Wei <chenwei@byzantine-lab.io>
2019-06-24 15:30:14 +0800

    lang setup,  lang see how it works

---
+++ src/constants/locales.js
@@ -0,0 +1,17 @@
+module.exports = {
+  default: {
+    path: '',
+    locale: 'English',
+    langCode: 'en',
+  },
+  english: {
+    path: 'en',
+    locale: 'English',
+    langCode: 'en',
+  },
+  'tranditional chinese': {
+    path: 'zh-Hant',
+    locale: '繁體中文',
+    langCode: 'zh-Hant',
+  },
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,18 +11,21 @@ const Test = styled.div`
   height: 20px;
 `;
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Test />
-    <h1>Hi people testest 2222</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ pathContext: { locale } }) => {
+  // console.log('locale', locale);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Test />
+      <h1>Hi people testest 2222</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+}
 
 export default IndexPage
