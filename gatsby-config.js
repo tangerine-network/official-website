758486b11e0be82fffeb979f061cf3b2d7bac562
Chen Wei <chenwei@byzantine-lab.io>
2019-08-26 12:34:06 +0800

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-143831791-2",
+        head: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
